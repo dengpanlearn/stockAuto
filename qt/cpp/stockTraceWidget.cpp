b17a349ba6f6e96eff99d8191e37d1296c84e8d3
dengpan <dengpanlearn@aliyun.com>
2019-10-11 17:35:46 +0800

    add stock trace widget

--- qt/cpp/stockTraceWidget.cpp
+++ qt/cpp/stockTraceWidget.cpp
@@ -1,10 +1,53 @@
+
+#include <qlabel.h>
+#include <qsplitter.h>
+#include <qtextcodec.h>
+#include <qtreewidget.h>
+#include <qlayout.h>
+#include <qwidget.h>
+#include "../include/qtResourceDef.h"
 #include "stockTraceWidget.h"
 
 CStockTraceWidget::CStockTraceWidget(QWidget *parent)
 	: QWidget(parent)
 {
+	OnInit();
+
+	Retranslate();
 }
 
 CStockTraceWidget::~CStockTraceWidget()
 {
+}
+
+
+void CStockTraceWidget::OnInit()
+{
+	QTextCodec* pCodec = QTextCodec::codecForLocale();
+	m_pWidgetWaitBuy = new QWidget();
+	m_pWidgetWaitBuy->setObjectName("Stock_Trace_Widget_Cell");
+
+	m_pWidgetWaitSell = new QWidget();
+	m_pWidgetWaitSell->setObjectName("Stock_Trace_Widget_Cell");
+
+	QLabel* pLabWaitBuy = new QLabel();
+	pLabWaitBuy->setText(pCodec->toUnicode("STOCK_TRACE_WIDET_LAB_WAIT_BUY"));
+	m_pTreeWaitBuy = new QTreeWidget();
+	QVBoxLayout* pLytWaitBuy = new QVBoxLayout();
+	pLytWaitBuy->addWidget(pLabWaitBuy);
+	pLytWaitBuy->addWidget(m_pTreeWaitBuy);
+	m_pWidgetWaitBuy->setLayout(pLytWaitBuy);
+
+	QLabel* pLabWaitSell = new QLabel();
+	pLabWaitSell->setText(pCodec->toUnicode("STOCK_TRACE_WIDET_LAB_WAIT_SELL"));
+	m_pTreeWaitSell = new QTreeWidget();
+	QVBoxLayout* pLytWaitSell = new QVBoxLayout();
+	pLytWaitSell->addWidget(pLabWaitSell);
+	pLytWaitSell->addWidget(m_pTreeWaitSell);
+	m_pWidgetWaitSell->setLayout(pLytWaitSell);
+}
+
+void CStockTraceWidget::Retranslate()
+{
+
 }
--- qt/include/stockTraceWidget.h
+++ qt/include/stockTraceWidget.h
@@ -9,7 +9,9 @@
 #include <QWidget>
 
 class CQtObjectAgent;
-
+class QTreeWidget;
+class QWidget;
+class QSplitter;
 
 class CStockTraceWidget : public QWidget
 {
@@ -20,14 +22,18 @@ public:
 	~CStockTraceWidget();
 
 private:
-//	void OnInit();
-//	void Retranslate();
+	void OnInit();
+	void Retranslate();
 
 private:
 	CQtObjectAgent*		m_pExitAgent;
 	CQtObjectAgent*		m_pStockAgent;
 
-
+	QTreeWidget*		m_pTreeWaitBuy;
+	QTreeWidget*		m_pTreeWaitSell;
+	QWidget*			m_pWidgetWaitBuy;
+	QWidget*			m_pWidgetWaitSell;
+	QSplitter*				m_pSplitter;
 };
 
 #endif // !__STOCK_TRACE_WIDGET_H__
